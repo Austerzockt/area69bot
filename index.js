89abbfddf866a736a3f6342277fe560f086dea73
Austerzockt <48381097+Austerzockt@users.noreply.github.com>
2021-01-20 13:01:18 +0100

    Fixed Split

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ client.on('message', message => {
     const args = split.slice(1);
 
     if (command.startsWith(prefix + "test")) {
-
         message.channel.send('pong')
         console.log(message.channel.messages.cache.toJSON())
-        // message.channel.send(message.channel.messages.cache.get('801397004915310652').content)
-        // message.channel.send(message.channel.messages.cache.get('801397004915310652').reactions.cache.toJSON())
+
+        if (args.length === 1) {
+            message.channel.send(message.channel.messages.cache.get(args[0]).content)
+            message.channel.send(message.channel.messages.cache.get(args[0]).reactions.cache.toJSON())
+        }
+
+
+
     }
 
 
