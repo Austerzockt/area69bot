831342e86e30763c442c33c9035f44395e544d7c
Austerzockt <48381097+Austerzockt@users.noreply.github.com>
2021-01-20 12:58:08 +0100

    Commit a Working (?) Bot

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ client.on('ready', () => {
 
     console.log('I am ready!');
 
+
 });
 
 
 
 client.on('message', message => {
+    const prefix = "+"
+    const split = message.content.split(" ");
+    const command = split_args[0]
+    const args = split_args.slice(1);
 
-            if (message.content.startsWith("!test")) {
-                message.channel.send('pong')
-                console.log(message.channel.messages.cache.toJSON())
-               // message.channel.send(message.channel.messages.cache.get('801397004915310652').content)
-                // message.channel.send(message.channel.messages.cache.get('801397004915310652').reactions.cache.toJSON())
-            }
+    if (command.startsWith(prefix + "test")) {
+
+        message.channel.send('pong')
+        console.log(message.channel.messages.cache.toJSON())
+        // message.channel.send(message.channel.messages.cache.get('801397004915310652').content)
+        // message.channel.send(message.channel.messages.cache.get('801397004915310652').reactions.cache.toJSON())
+    }
+
+
+
+
+
 
 
 
