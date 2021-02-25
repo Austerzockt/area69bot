846d2c815941ca00962ac0ae4d3e82a3f67899b8
Austerzockt <48381097+Austerzockt@users.noreply.github.com>
2021-02-25 12:47:48 +0100

    added auto kick for Luxi

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ client.on('guildMemberAdd', member => {
        }
    }
 });
+client.on('voiceStateUpdate', update => {
+    if (!update.channel.isText()) {
+        //641348606275747841
+        const  x =update.channel.members.find( user => user.id === "334595545060605955");
+        x.voice.kick().then();
+    }
+});
 
 client.on('message', message => {
     const prefix = "+"
