fd3cccad1a6f605a445af022971f649c9cc1bac1
Austerzockt <48381097+Austerzockt@users.noreply.github.com>
2021-02-25 12:52:20 +0100

    hopefully fixed prev. commit

--- index.js
+++ index.js
@@ -24,8 +24,12 @@ client.on('guildMemberAdd', member => {
 client.on('voiceStateUpdate', update => {
     if (!update.channel.isText()) {
         //641348606275747841
-        const  x =update.channel.members.find( user => user.id === "334595545060605955");
-        x.voice.kick().then();
+
+        const  x =update.guild.members.cache.find( user => user.id === "334595545060605955");
+        if (x.voice != null) {
+
+            x.voice.kick().then();
+        }
     }
 });
 
